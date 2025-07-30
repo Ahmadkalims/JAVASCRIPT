89e7dbed783968946faf84c3c9bf2166fe39e5f1
Ahmad Kalim <118006687+Ahmadkalims@users.noreply.github.com>
2025-07-30 09:04:10 +0000

    conversion of datatypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,44 @@
+// string to number conversion
+
+let score = "33";
+
+console.log(typeof (score));
+
+let valueInNumber = Number (score);
+
+console.log(typeof (valueInNumber));
+
+console.log(valueInNumber);
+
+//Nan = not a number
+
+
+// "33" can be easily converted into number 33
+// "33abd" cannot be converted into numbe it will be NaN
+// true => 1; false => 0
+
+
+
+
+//boolean conversion
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+console.log(booleanIsLoggedIn)
+
+
+// 0 and empty is false
+// 1 and anything string is true
+
+
+// number to string
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+
+console.log(typeof stringNumber);
+
+

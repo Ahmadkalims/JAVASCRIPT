37563a75146ead74f5e197da800ab27ffe99462a
Ahmad Kalim <118006687+Ahmadkalims@users.noreply.github.com>
2025-07-30 11:55:58 +0000

    maths 1

---
+++ 01_Basics/06_nums_and _maths.js
@@ -0,0 +1,9 @@
+const balance = new Number(100)
+console.log(balance)
+
+console.log(balance.toString().length) //3
+console.log(balance.toFixed(1))// 100.0
+
+const otherNumber = 123.23423
+console.log(otherNumber.toPrecision(3)) // be careful with toprecision
+
